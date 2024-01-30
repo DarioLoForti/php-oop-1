86ae8304567cdabd52c8081e8b1d40c7c92662fb
DarioLoForti <lofortidario@gmail.com>
2024-01-30 14:33:31 +0100

    add method to set oscar variable

--- index.php
+++ index.php
@@ -7,12 +7,22 @@ class Movie{
     public $director;
     public $duration;
     public $country_production;
+    public $oscar;
 
     function __construct($_title, $_genre, $_year){
         $this->title = $_title;
         $this->genre = $_genre;
         $this->year = $_year;
 
+    }
+    
+    public function setOscar($vote){
+        if($vote > 4){
+            $this->oscar = "Si";
+        }
+        else{
+            $this->oscar = "No";
+        }
     }
 
 }
@@ -22,8 +32,9 @@ $movie_1 = new Movie("Interstellar", "Fantacienza", "2014");
 $movie_1->director            = "Christopher Nolan";
 $movie_1->duration            = "168 minuti";
 $movie_1->country_production = "USA";
+$movie_1->setOscar(5);
 
-echo $movie_1->title." ".$movie_1->genre." ".$movie_1->year." ".$movie_1->director." ".$movie_1->duration." ".$movie_1->country_production;
+echo $movie_1->title." ".$movie_1->genre." ".$movie_1->year." ".$movie_1->director." ".$movie_1->duration." ".$movie_1->country_production." ".$movie_1->oscar;
 ?>
 
 
