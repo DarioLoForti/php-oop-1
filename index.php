3bb6a207ce4141b62924c3914c651fb529dd354a
DarioLoForti <lofortidario@gmail.com>
2024-01-30 13:36:57 +0100

    define movie class

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
+<?php
+
+class Movie{
+    public $titolo;
+    public $genre;
+    public $year;
+    public $director;
+    public $duration;
+    public $country_production;
+}
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
